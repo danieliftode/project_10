69862bfd0d53b3bc400cae5360d272fa3cfa887e
dany <contact@iftodedaniel.com>
2018-05-17 19:30:41 +0100

    final

--- script.js
+++ script.js
@@ -1,83 +1,103 @@
-var url = 'https://randomuser.me/api/?results=12';
-var tag = document.querySelectorAll('.user');
-var tag_one = document.querySelector('.user');
-var nume = document.querySelectorAll('.first');
-var numeDoi = document.querySelectorAll('.second');
-var mail = document.querySelectorAll('.email');
-var oras = document.querySelectorAll('.city');
-var pic = document.querySelectorAll('.poza');
-var modal = document.querySelector('.modal');
-var modal_cont = document.querySelector('.modal-content');
-//console.log(modAl_cont);
-function test(member){
-
-}
+//seting up the variables
+let api_link = 'https://randomuser.me/api/?results=12';
 let employees;
-var alpha = document.querySelector('body');
+const alpha = document.querySelector('body');
+//let fin = document.querySelector('.modal');
+let test = document.querySelector('body');
+let user_html = '';
+let key = 0;
+/*how ajax works:
+1 create a XMLHTTP request object
+2 build the callback function (what you want to do with the response from the ajax request to the server)
+3 open a request you give 2 information
+        a) the method you want the browser to get the information (get or post )
+        b) and the url WHERE THE REQUEST IS SENT ( the url of the api)
+4 send the request!!
+
+*/
+
+//step 1 make the ajax request and put the data inside div's and display
+//step 2 create a variable with html data for user in wich to insert api data
+//step 3 use interpolation!!!!
+//step 4 add a hey to each div created and insert a counter using interpolation
+//step 5 use that key to create a event if target clicked matches the user number display
+//the rest of the information about that user in a modal
 $(document).ready(function(){
-
-  function getInFo(url){
-    employees = url.results;
-    for(var i = 0; i < employees.length; i++){
-      for(var i = 0; i < nume.length; i++){
-        var capi_first = employees[i].name.first;
-        var capi_last = employees[i].name.last;
-        nume[i].innerText = capi_first.slice(0, 1).toUpperCase() + capi_first.slice(1, 20) + ' '
-        + capi_last.slice(0, 1).toUpperCase() + capi_last.slice(1, 10);
-      }
-    }
-
-    //making an object
-      // for(var i = 0; i < url.results.length; i++){
-      //   var user = {
-      //     aniversare : url.results[i].dob,
-      //     addressa : url.results[i].location,
-      //     telefonul : url.results[i].cell,
-      //     email : url.results[i].email,
-      //     user : url.results[i].login.username
-      //   }
-      // }
-
-
-      //need to add the data to each modal !!!
-    for (var i = 0; i < employees.length; i++){
-      for(var i = 0; i < mail.length; i++){
-        mail[i].innerText = employees[i].email;
-      }
-    }
-    for (var i = 0; i < employees.length; i++){
-      for(var i = 0; i < oras.length; i++){
-        oras[i].innerText = employees[i].location.city;
-      }
-    }
-    for (var i = 0; i < employees.length; i++){
-      for(var i = 0; i < pic.length; i++){
-        pic[i].setAttribute('src', employees[i].picture.large)
-      }
-    }
-    // schimb abordarea pun la fiecare user un modal individual in html structure
-    //si in el pun informatiile
-    tag_one.addEventListener('click', function(e){
-      if(e.target.classList[0] === 'user'){
-        modal.style.display = 'block';
-        modal_cont.innerHTML += '<h1>what the fuck</h1>';
-      }
-      if(e.target.classList[0] === 'close'){
-        modal.style.display = 'none';
-      }
-    });
+  function capital_letter(word){
+    word = word.slice(0, 1).toUpperCase() + word.slice(1, 100);
+    return word;
   }
+//build the html with esential data
+  function call_me_back(api_link){
+    employees = api_link.results;
+    for(var i=0; i < employees.length; i++){
+      alpha.innerHTML += `<div data-tag=${key} class="user">
+                        <img class="poza" src=${employees[i].picture.large} alt="poza"/>
+                        <div class="detalii">
+                        <h3 class="name">${capital_letter(employees[i].name.first)}   ${capital_letter(employees[i].name.last)}</h3>
+                        <p class= 'email'>${employees[i].email}</p>
+                        <p class= 'city'>${capital_letter(employees[i].location.city)}</p>
+      </div>
+      </div>` ;
+      key++ //counter for modal
 
-  //for tag
-  $.getJSON(url, getInFo);
-// alpha.addEventListener('click', function(e){
-//   if(e.target.classList[0]  === 'user') {
-//     modAl.style.display = 'block';
-//   }
-//   if(e.target.classList[0] === 'close'){
-//     modAl.style.display = 'none';
-//   }
-// })
+    }
+    let person = document.querySelectorAll('.user');
+    let single_person = document.querySelector('.user');
+    var modal = document.querySelector('.modal');
+    var modal_off = document.querySelector('.close');
+    var content = document.querySelector('.modal-content');
+    //need to add event listener to close the modal
+
+    //finish of the closing of the modal
+    //need to add event listener on user and see if scope of employees[i] exists in event handler
+    for(var x = 0; x<person.length; x++){
+      person[x].addEventListener('click', function(e){
+
+        modal.style.display = "block";
+        // for(var i = 0; i < employees.length; i++){
+        //    let human = {
+        //     number : employees
+        //   }
+        // }
+        var place = this.dataset.tag;
+        //if(e.target.dataset.tag === this.dataset.tag){ //if user click matches user data tag
+
+          // content.innerHTML = "<span class='close'>&times;</span>" +
+          // "<img src=" + employees[place].picture.large + ">" +
+          // "<h3>" + capital_letter(employees[place].name.first) + "  " + capital_letter(employees[place].name.last) + "</h3>" +
+          // "<h3> User Name : " + employees[place].login.username + "</h3>" +
+          // "<h3> Email : " + employees[place].email + "</h3>" +
+          // "<h3> Telephone : " + employees[place].cell + "</h3>" +
+          // "<h3> Address : " + employees[place].location.street + " , " + employees[place].location.city + " , " +   + employees[place].location.postcode + "</h3>" +
+          // "<h3> Birthdate : " + employees[place].dob + "</h3>";
 
 
-})//end ready
+          // rearanged
+          content.innerHTML = "<span class='close'>&times;</span>" +
+          "<img src=" + employees[place].picture.large + ">" +
+          "<h3>" + capital_letter(employees[place].name.first) + "  " + capital_letter(employees[place].name.last) + "</h3>" +
+          "<h4> " + employees[place].email + "</h4>" +
+          "<h4> " + capital_letter(employees[place].location.city) + "</h4>" +
+          "<h4> " + employees[place].cell + "</h4>" +
+          "<h4> " + employees[place].location.street + " , " + capital_letter(employees[place].location.state) + " " + employees[place].location.postcode + "</h4>" +
+          "<h4> Birthday : " +  employees[place].dob + "</h4>";
+        //}
+        //+ employees[place].picture.large
+
+      });
+      modal.addEventListener('click', function(e){
+        if(e.target){
+          modal.style.display = "none";
+        }
+      });
+    }
+    //trying to make a call back to get all user details and stor it
+
+
+    //++++++++++++++++++++++++++++++
+    //im experimenting with shit
+    }
+
+  $.getJSON(api_link, call_me_back);
+});
